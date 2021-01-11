0bbb798d6857b8fccd6764b80ad861e9cfabe5ad
Peko <zh0208@163.com>
2021-01-12 00:10:52 +0800

    User.cs Created

---
+++ User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employees_Management_System
+{
+    public class User
+    {
+        public Int32 user_Id { get; set; }
+        public String user_Name { get; set; }
+        public String user_Password { get; set; }
+
+        public override string ToString()
+        {
+            return "{user_id}    |    {user_Name}    |    {user_Password}";
+        }
+    }
+}
